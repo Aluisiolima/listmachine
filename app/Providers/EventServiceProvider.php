ada8a498f2404b7060b81949477c17f48b139951
Aluisio Lima <aaluisio309@gmail.com>
2025-11-27 16:11:52 -0300

    fix(): esqueci de declara

--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -3,7 +3,19 @@
 namespace App\Providers;
 
 use App\Models\Computer;
+use App\Models\HardwareComponents;
+use App\Models\Locations;
+use App\Models\MaintenanceRecords;
+use App\Models\Qrcode;
+use App\Models\Software;
+use App\Models\User;
 use App\Observers\ComputerObserver;
+use App\Observers\HardwareComponentsObeserver;
+use App\Observers\LocationsObeserver;
+use App\Observers\MaintenanceRecordsObeserver;
+use App\Observers\QrcodeObeserver;
+use App\Observers\SoftwareObeserver;
+use App\Observers\UserObserver;
 use Illuminate\Support\ServiceProvider;
 
 class EventServiceProvider extends ServiceProvider
@@ -22,5 +34,11 @@ class EventServiceProvider extends ServiceProvider
     public function boot(): void
     {
         Computer::observe(ComputerObserver::class);
+        HardwareComponents::observe(HardwareComponentsObeserver::class);
+        Locations::observe(LocationsObeserver::class);
+        MaintenanceRecords::observe(MaintenanceRecordsObeserver::class);
+        Qrcode::observe(QrcodeObeserver::class);
+        Software::observe(SoftwareObeserver::class);
+        User::observe(UserObserver::class);
     }
 }
